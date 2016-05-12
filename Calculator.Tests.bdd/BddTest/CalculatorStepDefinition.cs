08394ab088e9767014549f140a5c5692b564bf47
Consultant <consultant@bk-consulting.com>
2016-05-12 13:57:08 +0200

    improve specflow test speed

--- Calculator.Tests.bdd/BddTest/CalculatorStepDefinition.cs
+++ Calculator.Tests.bdd/BddTest/CalculatorStepDefinition.cs
@@ -14,22 +14,22 @@ namespace Calculator.Tests.Features
     [Binding]
     public sealed class CalculatorStepDefinition
     {
-        private IWebDriver _driver;
-        private IndexPage _indexPage;
+        private static IWebDriver _driver;
+        private static IndexPage _indexPage;
         private CalculatorItems calc = new CalculatorItems();
-        private string calculateResult = string.Empty;
+        private static string calculateResult = string.Empty;
 
         // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
-        [BeforeScenario]
-        public void ScenarioSetUp()
+        [BeforeFeature]
+        public static void ScenarioSetUp()
         {
             _driver = new DriverFactory().Create();
             _indexPage = new IndexPage(_driver);
             _driver.Navigate().GoToUrl("http://localhost:8080/");
         }
 
-        [AfterScenario]
-       public void ScenarioTearDown()
+        [AfterFeature]
+        public static void ScenarioTearDown()
         {
             try
             {
@@ -42,13 +42,12 @@ namespace Calculator.Tests.Features
             }
         }
 
-
         [Given("I have entered (.*) into the first operand of the calculator")]
         public void GivenIHaveEnteredSomethingIntoFirstOperandOfTheCalculator(double number)
         {
+            _driver.Navigate().GoToUrl("http://localhost:8080/");
             _indexPage.Item1.SendKeys(number.ToString());
         }
-
 
         [Given("I have choose (.*) as an operation into the calculator")]
         public void GivenIHaveEnteredSomethingIntoTheCalculator(string Operator)
